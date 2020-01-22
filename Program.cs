16401b87be8b7486eacc1106bba7596c4bccb79c
Márk Diószegi <walnut5588@gmail.com>
2020-01-22 17:43:30 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
